c9f9bf770ba61ff8b77327a895c224e7b1afaf73
Shamus Taylor <Shamus03@me.com>
2019-10-12 18:56:02 -0500

    move update hook

--- src/registerServiceWorker.ts
+++ src/registerServiceWorker.ts
@@ -12,11 +12,6 @@ if (process.env.NODE_ENV === 'production') {
         'App is being served from cache by a service worker.\n' +
         'For more details, visit https://goo.gl/AFskqB'
       )
-      navigator.serviceWorker.addEventListener('controllerchange', () => {
-        if (confirm('New version available!  Refresh?')) {
-          window.location.reload()
-        }
-      })
     },
     registered () {
       console.log('Service worker has been registered.')
@@ -29,6 +24,11 @@ if (process.env.NODE_ENV === 'production') {
     },
     updated () {
       console.log('New content is available; please refresh.')
+      navigator.serviceWorker.addEventListener('controllerchange', () => {
+        if (confirm('New version available!  Refresh?')) {
+          window.location.reload()
+        }
+      })
     },
     offline () {
       console.log('No internet connection found. App is running in offline mode.')
