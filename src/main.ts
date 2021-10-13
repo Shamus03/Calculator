0b52805e6a4939697c7d65331bc56ff6b7b33563
Shamus Taylor <Shamus03@me.com>
2021-10-12 19:22:05 -0500

    Use fastclick

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 
+import fastclick from 'fastclick'
+if ('addEventListener' in document) {
+  document.addEventListener('DOMContentLoaded', function () {
+    fastclick(document.body)
+  }, false)
+}
+
 Vue.config.productionTip = false
 
 new Vue({
